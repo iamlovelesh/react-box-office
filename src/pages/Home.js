1a66e2f6a04e57e7cb2b6af53d1e40049f619329
iamlovelesh <rghvn29@gmail.com>
2021-07-10 11:36:07 +0530

    Added Input and API fetch
    Signed-off-by: iamlovelesh <rghvn29@gmail.com>

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import MainPageLayout from "../components/MainPageLayout";
 
 const Home = () => {
+  const [input, setInput] = useState("");
+  const onInputChange = (e) => {
+    setInput(e.target.value);
+  };
+  const onSearch = () => {
+    fetch(`https://api.tvmaze.com/singlesearch/shows?q=${input}`)
+      .then((res) => res.json())
+      .then((result) => {
+        console.log(result);
+      });
+  };
+  const onKeyDown = (e) => {
+    if (e.keyCode === 13) {
+      onSearch();
+    }
+  };
   return (
     <MainPageLayout>
-      <h2>Welcome to the Home Page</h2>
+      <input
+        type="text"
+        onChange={onInputChange}
+        onKeyDown={onKeyDown}
+        value={input}
+      />
+      <button type="button" onClick={onSearch}>
+        Search
+      </button>
     </MainPageLayout>
   );
 };
