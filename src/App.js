c12ffd80688789810e866dc845b8e8275e3f3da9
iamlovelesh <rghvn29@gmail.com>
2021-07-10 10:49:38 +0530

    Added Components and Pages
    Signed-off-by: iamlovelesh <rghvn29@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import React from "react";
-
+import { Switch, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Starred from "./pages/Starred";
+import Home from "./pages/Home";
 function App() {
-  return <div></div>;
+  return (
+    <Switch>
+      <Route exact path="/starred">
+        <Starred />
+      </Route>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route>
+        <div>404! Page Not Found</div>
+      </Route>
+    </Switch>
+  );
 }
 
 export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import MainPageLayout from "../components/MainPageLayout";
+
+const Home = () => {
+  return (
+    <MainPageLayout>
+      <h2>Welcome to the Home Page</h2>
+    </MainPageLayout>
+  );
+};
+
+export default Home;
