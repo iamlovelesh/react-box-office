ccb5d2d0a142eec9671f56ddce833b756eaf24db
iamlovelesh <rghvn29@gmail.com>
2021-07-11 07:52:07 +0530

    Added Loading and Error Messages
    Signed-off-by: iamlovelesh <rghvn29@gmail.com>

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -4,14 +4,46 @@ import { apiGet } from "../misc/config";
 const Show = () => {
   const { id } = useParams();
   const [show, setShow] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
   useEffect(() => {
-    apiGet(`shows/${id}?embed[]=seasons&embed[]=cast`).then((result) =>
-      setShow(result)
-    );
-    console.log(show);
-    return () => {};
+    let isMounted = true;
+    apiGet(`shows/${id}?embed[]=seasons&embed[]=cast`)
+      .then((result) => {
+        if (isMounted) {
+          setShow(result);
+          setIsLoading(false);
+        }
+      })
+      .catch((err) => {
+        if (isMounted) {
+          setError(err.message);
+          setIsLoading(false);
+        }
+      });
+    return () => {
+      isMounted = false;
+    };
   }, [id, show]);
-  return <div>This is Show Page</div>;
+  if (isLoading) {
+    return (
+      <div>
+        <h2>Loading</h2>
+      </div>
+    );
+  }
+  if (error) {
+    return (
+      <div>
+        <h3>{error}</h3>
+      </div>
+    );
+  }
+  return (
+    <div>
+      <h1>This is the Show Page</h1>
+    </div>
+  );
 };
 
 export default Show;
