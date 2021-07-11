a661ec98cca30625e6812a5cc2f9486ad94e8ba7
iamlovelesh <rghvn29@gmail.com>
2021-07-11 09:24:47 +0530

    Added Show Page Components
    Signed-off-by: iamlovelesh <rghvn29@gmail.com>

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,6 +1,10 @@
 import React, { useEffect, useReducer } from "react";
 import { useParams } from "react-router-dom";
 import { apiGet } from "../misc/config";
+import ShowMainData from "../components/show/ShowMainData";
+import Details from "../components/show/Details";
+import Cast from "../components/show/Cast";
+import Seasons from "../components/show/Seasons";
 const reducer = (prevState, action) => {
   switch (action.type) {
     case "FETCH_SUCCESS": {
@@ -57,7 +61,29 @@ const Show = () => {
   }
   return (
     <div>
-      <h1>This is the Show Page</h1>
+      <ShowMainData
+        image={show.image}
+        name={show.name}
+        rating={show.rating}
+        summary={show.summary}
+        tags={show.genres}
+      />
+      <div>
+        <h2>Details</h2>
+        <Details
+          status={show.status}
+          network={show.network}
+          premiered={show.premiered}
+        />
+      </div>
+      <div>
+        <h2>Seasons</h2>
+        <Seasons seasons={show._embedded.seasons} />
+      </div>
+      <div>
+        <h2>Cast</h2>
+        <Cast cast={show._embedded.cast} />
+      </div>
     </div>
   );
 };
